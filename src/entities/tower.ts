4a1da808627ee1b067396a43963b6e722f02c967
Daniel Jackson <daniel@cloakedninjas.co.uk>
2019-04-28 12:38:40 +0300

    Spiders face + shoot web

--- src/entities/tower.ts
+++ src/entities/tower.ts
@@ -1,19 +1,31 @@
 import Scene = Phaser.Scene;
 import {Structure} from "./structure";
 import {Hero} from "./hero";
+import Sprite = Phaser.GameObjects.Sprite;
 
 export class Tower extends Structure {
   placed: boolean;
   canAttack: boolean;
+  web: Sprite;
 
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, 'tower');
 
     this.attackRange = 2;
+    this.web = new Sprite(scene, x, y, 'tower');
+    //this.web.setVisible(false);
+    this.web.tint = 0x334455;
+    this.web.setScale(0.3);
+    scene.add.existing(this.web);
+
     this.refreshAttack();
   }
 
   trigger(hero: Hero) {
+    const angle = Phaser.Math.Angle.Between(this.x, this.y, hero.x, hero.y);
+
+    this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
+
     if (this.canAttack) {
       this.attack(hero);
       this.canAttack = false;
@@ -25,11 +37,26 @@ export class Tower extends Structure {
   }
 
   attack(hero: Hero) {
-    console.log('Slow...');
     hero.slow();
+
+    this.web.setPosition(this.x, this.y);
+    this.web.setVisible(true);
+
+    this.scene.tweens.add({
+      targets: this.web,
+      x: hero.x,
+      y: hero.y,
+      ease: 'Quad.easeIn',
+      duration: 200,
+      onComplete: () => {
+        this.web.setVisible(false);
+      }
+    });
   }
 
   refreshAttack() {
     this.canAttack = true;
   }
+
+
 }
